47465f5b9b8b57202be06f4b8b04809de2d6f708
= <=>
2023-10-31 22:59:25 -0700

    Added tab navigation

--- frontend/src/components/OptionBar.jsx
+++ frontend/src/components/OptionBar.jsx
@@ -13,8 +13,9 @@ import {
   Popover,
   PopoverSurface,
   PopoverTrigger,
+  Tab,
+  TabList,
   Toolbar,
-  ToolbarButton,
   ToolbarDivider,
   makeStyles,
   tokens,
@@ -43,22 +44,32 @@ const useStyles = makeStyles({
     zIndex: "7",
     backgroundColor: tokens.colorBrandBackground2,
     paddingTop: "10px",
-    paddingBottom: "10px",
+    paddingRight: "10px",
+    paddingLeft: "10px",
+    paddingBottom: "15px",
     display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    "> div": {
-      display: "flex",
-      flexWrap: "wrap",
-      rowGap: "15px",
-    },
-    "> div > .select-options": {
-      display: "none",
-    },
+    flexDirection: "column",
   },
-  control: {
-    maxWidth: "300px",
+  tabContainer: {
+    width: "fit-content",
+    marginTop: "10px",
+    backgroundColor: tokens.colorNeutralBackground2,
+    display: "flex",
+    flexWrap: "wrap",
+    rowGap: "15px",
+    paddingLeft: "10px",
+    paddingTop: "10px",
+    paddingBottom: "10px",
+    paddingRight: "10px",
+    borderTopRightRadius: "8px",
+    borderTopLeftRadius: "8px",
+    borderBottomRightRadius: "8px",
+    borderBottomLeftRadius: "8px",
+    boxShadow: tokens.shadow8
   },
+  selectOptions: {
+    display: "none"
+  }
 });
 
 const OptionBar = () => {
@@ -68,11 +79,18 @@ const OptionBar = () => {
     { value: "apat", text: "Apellido Paterno" },
     { value: "amat", text: "Apellido Materno" },
   ];
-  const documents = [{value: "titulo", text: "Título"}, {value: "cert", text: "Certificado"}];
-  const sex = [{value: "M", text: "Masculino"}, {value: "F", text: "Femenino"}];
+  const documents = [
+    { value: "titulo", text: "Título" },
+    { value: "cert", text: "Certificado" },
+  ];
+  const careerList = careers;
+  const sex = [
+    { value: "M", text: "Masculino" },
+    { value: "F", text: "Femenino" },
+  ];
   const initialState = {
     criteria: [searchCriteria[0].value],
-    careers: careers.map(({ value }) => value),
+    careers: careerList.map(({ value }) => value),
     sex: ["M", "F"],
     docs: ["titulo", "cert"],
   };
@@ -85,6 +103,12 @@ const OptionBar = () => {
     });
   };
 
+  const [selectedTabValue, setSelectedTabValue] = useState("main");
+
+  const onTabSelect = (_, data) => {
+    setSelectedTabValue(data.value);
+  };
+
   const menu = (list, title, groupName, icon, multiselect) => {
     return (
       <Menu checkedValues={checkedValues} onCheckedValueChange={onChange}>
@@ -95,106 +119,122 @@ const OptionBar = () => {
         </MenuTrigger>
         <MenuPopover>
           <MenuList>
-            {multiselect ? (
-              list.map(({ value, text }, index) => (
-                <MenuItemCheckbox key={index} name={groupName} value={value}>
-                  {text}
-                </MenuItemCheckbox>
-              ))
-            ) : (
-              list.map(({ value, text }, index) => (
-                <MenuItemRadio key={index} name={groupName} value={value}>
-                  {text}
-                </MenuItemRadio>
-              ))
-            )}
+            {multiselect
+              ? list.map(({ value, text }, index) => (
+                  <MenuItemCheckbox key={index} name={groupName} value={value}>
+                    {text}
+                  </MenuItemCheckbox>
+                ))
+              : list.map(({ value, text }, index) => (
+                  <MenuItemRadio key={index} name={groupName} value={value}>
+                    {text}
+                  </MenuItemRadio>
+                ))}
           </MenuList>
         </MenuPopover>
       </Menu>
     );
   };
 
-  const styles = useStyles();
-
-  return (
-    <>
-      <section className={styles.navContainer}>
-        <Toolbar as="div" size="large">
-          <Input appearance="underline" contentBefore={<Search16Regular />} />
-          {menu(
-            searchCriteria,
-            null,
-            "criteria",
-            <MoreHorizontal16Regular />,
-            false
-          )}
-          <ToolbarDivider />
-          {menu(careers, "Carreras", "careers", null, true)}
-          {menu(documents, "Documento", "docs", <Document16Regular />, true)}
-          {menu(sex, "Sexo", "sex", <People16Regular />, true)}
-          <Popover
-            withArrow
-            trapFocus
-            open={open === "first"}
-            onOpenChange={(_, data) => setOpen(data.open ? "first" : undefined)}
-          >
-            <PopoverTrigger disableButtonEnhancement>
-              <Button appearance="transparent" icon={<Calendar16Regular />}>
-                Fecha
-              </Button>
-            </PopoverTrigger>
-            <PopoverSurface>
-              <Field label={"Desde"}>
-                <DatePicker
-                  className={styles.control}
-                  allowTextInput
-                  placeholder="Fecha de inicio"
-                />
-              </Field>
-              <br />
-              <Field label={"Hasta"}>
-                <DatePicker
-                  className={styles.control}
-                  allowTextInput
-                  placeholder="Fecha de fin"
-                />
-              </Field>
-            </PopoverSurface>
-          </Popover>
-          <ToolbarDivider />
-          <Button appearance="transparent" icon={<Add16Filled />}>
-            Agregar
-          </Button>
-          <Button appearance="transparent" icon={<CheckboxChecked16Regular />}>
-            Seleccionar
-          </Button>
-          <ToolbarDivider />
-
-          <div className="select-options">
-            <Button appearance="transparent" icon={<Edit16Regular />}></Button>
-            <Button appearance="transparent" icon={<Delete16Filled />}></Button>
-            <Button appearance="transparent" icon={<Dismiss16Filled />}>
-              Cerrar Selección
+  const filterTab = () => {
+    return (
+      <Toolbar className={styles.tabContainer} size="large">
+        {menu(careerList, "Carreras", "careers", null, true)}
+        {menu(documents, "Documento", "docs", <Document16Regular />, true)}
+        {menu(sex, "Sexo", "sex", <People16Regular />, true)}
+        <Popover
+          withArrow
+          trapFocus
+          open={open === "first"}
+          onOpenChange={(_, data) => setOpen(data.open ? "first" : undefined)}
+        >
+          <PopoverTrigger disableButtonEnhancement>
+            <Button appearance="transparent" icon={<Calendar16Regular />}>
+              Fecha
             </Button>
-          </div>
-          
-          <Menu>
-            <MenuTrigger disableButtonEnhancement>
-              <MenuButton appearance="primary" icon={<Save16Regular />}>
-                Guardar como
-              </MenuButton>
-            </MenuTrigger>
-            <MenuPopover>
-              <MenuList>
-                <MenuItem>PDF</MenuItem>
-                <MenuItem>Excel</MenuItem>
-              </MenuList>
-            </MenuPopover>
-          </Menu>
-        </Toolbar>
-      </section>
-    </>
-  );
+          </PopoverTrigger>
+          <PopoverSurface>
+            <Field label={"Desde"}>
+              <DatePicker
+                className={styles.control}
+                allowTextInput
+                placeholder="Fecha de inicio"
+              />
+            </Field>
+            <br />
+            <Field label={"Hasta"}>
+              <DatePicker
+                className={styles.control}
+                allowTextInput
+                placeholder="Fecha de fin"
+              />
+            </Field>
+          </PopoverSurface>
+        </Popover>
+      </Toolbar>
+    );
+  };
+
+  const mainTab = () => {
+    return (
+      <Toolbar className={styles.tabContainer} size="large">
+        <Input appearance="underline" contentBefore={<Search16Regular />} />
+        {menu(
+          searchCriteria,
+          null,
+          "criteria",
+          <MoreHorizontal16Regular />,
+          false
+        )}
+        <ToolbarDivider />
+        <Button as="a" href="/dashboard/import" appearance="transparent" icon={<Add16Filled />}>
+          Agregar
+        </Button>
+        <Button appearance="transparent" icon={<CheckboxChecked16Regular />}>
+          Seleccionar
+        </Button>
+        <ToolbarDivider />
+
+        <div className={styles.selectOptions}>
+          <Button appearance="transparent" icon={<Edit16Regular />}></Button>
+          <Button appearance="transparent" icon={<Delete16Filled />}></Button>
+          <Button appearance="transparent" icon={<Dismiss16Filled />}>
+            Cerrar Selección
+          </Button>
+        </div>
+
+        <Menu>
+          <MenuTrigger disableButtonEnhancement>
+            <MenuButton appearance="primary" icon={<Save16Regular />}>
+              Guardar como
+            </MenuButton>
+          </MenuTrigger>
+          <MenuPopover>
+            <MenuList>
+              <MenuItem>PDF</MenuItem>
+              <MenuItem>Excel</MenuItem>
+            </MenuList>
+          </MenuPopover>
+        </Menu>
+      </Toolbar>
+    );
+  };
+
+  const styles = useStyles();
+  return (<section className={styles.navContainer}>
+    <TabList  selectedValue={selectedTabValue} onTabSelect={onTabSelect}>
+        <Tab id="main" value="main">
+          Inicio
+        </Tab>
+        <Tab id="filter" value="filter">
+          Filtros
+        </Tab>
+      </TabList>
+      <div>
+        {selectedTabValue === "main" && mainTab()}
+        {selectedTabValue === "filter" && filterTab()}
+      </div>
+  </section>);
 };
 
 export default OptionBar;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { FluentProvider, webDarkTheme } from "@fluentui/react-components";
+import { FluentProvider, webDarkTheme, webLightTheme } from "@fluentui/react-components";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <FluentProvider theme={webDarkTheme}>
+    <FluentProvider theme={webLightTheme}>
       <App />
     </FluentProvider>
   </React.StrictMode>
