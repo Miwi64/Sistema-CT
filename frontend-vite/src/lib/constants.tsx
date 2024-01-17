ffd8e8b594157e86bba9ae5780cf8fe213d07104
Miwi64 <miargote@hotmail.com>
2024-01-16 19:40:17 -0800

    Added certificate form input fields. TextArea Component.  Created constants file.

---
+++ frontend-vite/src/lib/constants.tsx
@@ -0,0 +1,8 @@
+export const CARREERS = [
+    { value: "sistemas", text: "Sistemas" },
+    { value: "administracion", text: "Administración" },
+    { value: "agricola", text: "Agrícola" },
+    { value: "industrial", text: "Industrial" },
+    { value: "mecatronica", text: "Mecatrónica" },
+    { value: "electromecanica", text: "Electromecánica" },
+]
