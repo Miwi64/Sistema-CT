0cafaad98ac7f7773821ebd6cc984b4c58a41083
Miwi64 <miargote@hotmail.com>
2024-01-16 22:07:57 -0800

    Added and validated student fields

--- frontend-vite/src/lib/constants.tsx
+++ frontend-vite/src/lib/constants.tsx
@@ -6,3 +6,16 @@ export const CARREERS = [
     { value: "mecatronica", text: "Mecatrónica" },
     { value: "electromecanica", text: "Electromecánica" },
 ]
+
+export const STATES = [
+    { text: 'Aguascalientes', value: 'aguascalientes' },
+    { text: 'Baja California', value: 'baja_california' },
+    { text: 'Baja California Sur', value: 'baja_california_sur' },
+    { text: 'Campeche', value: 'campeche' },
+    { text: 'Chiapas', value: 'chiapas' },
+    { text: 'Chihuahua', value: 'chihuahua' },
+    { text: 'Coahuila', value: 'coahuila' },
+    { text: 'Colima', value: 'colima' },
+    { text: 'Durango', value: 'durango' },
+    { text: 'Guanajuato', value: 'guanajuato' }
+]
--- frontend-vite/src/screens/certificate-form.tsx
+++ frontend-vite/src/screens/certificate-form.tsx
@@ -1,5 +1,5 @@
 import PageLayout from "@/components/page-layout";
-import StudentForm from "@/components/student-form";
+import StudentFields from "@/components/student-fields";
 import { Button } from "@/components/ui/button";
 import { Calendar } from "@/components/ui/calendar";
 import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
@@ -17,15 +17,19 @@ import * as z from "zod"
 
 const formSchema = z.object({
   num_control: z.string().min(8, "Mínimo 8 caracteres").max(10, "Máximo 10 caracteres"),
-  nombre: z.string().max(48, "Máximo 48 caracteres"),
-  apellidop: z.string().max(50, "Máximo 50 caracteres"),
+  nombre: z.string({ required_error: "Campo requerido" }).transform(value => value.replace(/\s+/g, ''))
+    .pipe(z.string().min(1, { message: "Campo requerido" }).max(50, "Máximo 50 caracteres")),
+  apellidop: z.string({ required_error: "Campo requerido" }).transform(value => value.replace(/\s+/g, ''))
+    .pipe(z.string().min(1, { message: "Campo requerido" }).max(50, "Máximo 50 caracteres")),
   apellidom: z.string().max(50, "Máximo 50 caracteres").optional(),
-  sexo: z.string().max(1),
-  CURP: z.string().regex(/^([A-Z][AEIOUX][A-Z]{2}\d{2}(?:0[1-9]|1[0-2])(?:0[1-9]|[12]\d|3[01])[HM](?:AS|B[CS]|C[CLMSH]|D[FG]|G[TR]|HG|JC|M[CNS]|N[ETL]|OC|PL|Q[TR]|S[PLR]|T[CSL]|VZ|YN|ZS)[B-DF-HJ-NP-TV-Z]{3}[A-Z\d])(\d)$/, "CURP inválido"),
-  estado_nacimiento: z.string(),
-  fecha_nacimiento: z.coerce.date(),
-  periodo_ingreso: z.string(),
-  periodo_egreso: z.string(),
+  sexo: z.string({ required_error: "Campo requerido" }).max(1),
+  CURP: z.string({ required_error: "Campo requerido" }).regex(/^([A-Z][AEIOUX][A-Z]{2}\d{2}(?:0[1-9]|1[0-2])(?:0[1-9]|[12]\d|3[01])[HM](?:AS|B[CS]|C[CLMSH]|D[FG]|G[TR]|HG|JC|M[CNS]|N[ETL]|OC|PL|Q[TR]|S[PLR]|T[CSL]|VZ|YN|ZS)[B-DF-HJ-NP-TV-Z]{3}[A-Z\d])(\d)$/, "CURP inválido"),
+  estado_nacimiento: z.string({ required_error: "Campo requerido" }),
+  fecha_nacimiento: z.date({ required_error: "Campo requerido" }),
+  periodo_ingreso: z.string({ required_error: "Campo requerido" }).transform(value => value.replace(/\s+/g, ''))
+    .pipe(z.string().max(50, "Máximo 50 caracteres")),
+  periodo_egreso: z.string({ required_error: "Campo requerido" }).transform(value => value.replace(/\s+/g, ''))
+    .pipe(z.string().max(50, "Máximo 50 caracteres")),
   num_folio: z.string({ required_error: "Campo requerido" }).transform(value => value.replace(/\s+/g, ''))
     .pipe(z.string().min(1, { message: "Campo requerido" })),
   carrera: z.string({ required_error: "Campo requerido" }),
@@ -36,18 +40,6 @@ const formSchema = z.object({
 const CertificateForm = () => {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
-    defaultValues: {
-      num_control: "",
-      nombre: "",
-      apellidop: "",
-      apellidom: "",
-      sexo: "",
-      CURP: "",
-      estado_nacimiento: "",
-      fecha_nacimiento: new Date("1940-01-01T07:00:00.128Z"),
-      periodo_ingreso: "",
-      periodo_egreso: "",
-    },
   })
 
   const onSubmit = (values: z.infer<typeof formSchema>) => {
@@ -59,109 +51,114 @@ const CertificateForm = () => {
   return (
     <PageLayout>
       <h1 className="my-5 text-2xl font-semibold leading-none tracking-tight">Agregar certificado</h1>
-      <section className="max-w-[500px]">
+      <section className="">
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)}>
-            <h2 className="text-lg leading-none tracking-tight">Datos del estudiante</h2>
-            <StudentForm form={form} />
-            <h2 className="text-lg leading-none tracking-tight">Datos del certificado</h2>
-            <FormField
-              control={form.control}
-              name="num_folio"
-              render={({ field }) => (
-                <FormItem className="my-5">
-                  <FormLabel>Número de folio</FormLabel>
-                  <FormControl>
-                    <Input placeholder="Número de folio" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="carrera"
-              render={({ field }) => (
-                <FormItem className="my-5">
-                  <FormLabel>Carrera</FormLabel>
-                  <Select onValueChange={field.onChange} defaultValue={field.value}>
+            <h2 className="my-5 text-lg leading-none tracking-tight">Datos del estudiante</h2>
+            <section className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+              <StudentFields form={form} />
+            </section>
+            <h2 className="my-5 text-lg leading-none tracking-tight">Datos del certificado</h2>
+            <section className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+              <FormField
+                control={form.control}
+                name="num_folio"
+                render={({ field }) => (
+                  <FormItem className="">
+                    <FormLabel>Número de folio</FormLabel>
                     <FormControl>
-                      <SelectTrigger>
-                        <SelectValue placeholder="Seleccionar carrera" />
-                      </SelectTrigger>
+                      <Input placeholder="Número de folio" {...field} />
                     </FormControl>
-                    <SelectContent>
-                      {CARREERS.map((
-                        { value, text }, key) =>
-                        (<SelectItem value={value}>{text}</SelectItem>))}
-                    </SelectContent>
-                  </Select>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="fecha_registro_cert"
-              render={({ field }) => (
-                <FormItem className="flex flex-col my-5">
-                  <FormLabel>Fecha de registro</FormLabel>
-                  <Popover>
-                    <PopoverTrigger asChild>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="carrera"
+                render={({ field }) => (
+                  <FormItem className="">
+                    <FormLabel>Carrera</FormLabel>
+                    <Select onValueChange={field.onChange} defaultValue={field.value}>
                       <FormControl>
-                        <Button
-                          variant={"outline"}
-                          className={cn(
-                            "pl-3 text-left font-normal",
-                            !field.value && "text-muted-foreground"
-                          )}
-                        >
-                          {field.value ? (
-                            format(field.value, "PPP")
-                          ) : (
-                            <span>Fecha</span>
-                          )}
-                          <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
-                        </Button>
+                        <SelectTrigger>
+                          <SelectValue placeholder="Seleccionar carrera" />
+                        </SelectTrigger>
                       </FormControl>
-                    </PopoverTrigger>
-                    <PopoverContent className="w-auto p-0" align="start">
-                      <Calendar
-                        mode="single"
-                        selected={field.value}
-                        onSelect={field.onChange}
-                        disabled={(date) =>
-                          date > new Date() || date < new Date("1900-01-01")
-                        }
-                        initialFocus
+                      <SelectContent>
+                        {CARREERS.map((
+                          { value, text }, key) =>
+                          (<SelectItem value={value}>{text}</SelectItem>))}
+                      </SelectContent>
+                    </Select>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="fecha_registro_cert"
+                render={({ field }) => (
+                  <FormItem className="flex flex-col gap-2">
+                    <FormLabel>Fecha de registro</FormLabel>
+                    <Popover>
+                      <PopoverTrigger asChild>
+                        <FormControl>
+                          <Button
+                            variant={"outline"}
+                            className={cn(
+                              "pl-3 text-left font-normal",
+                              !field.value && "text-muted-foreground"
+                            )}
+                          >
+                            {field.value ? (
+                              format(field.value, "PPP")
+                            ) : (
+                              <span>Fecha</span>
+                            )}
+                            <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                          </Button>
+                        </FormControl>
+                      </PopoverTrigger>
+                      <PopoverContent className="w-auto p-0" align="start">
+                        <Calendar
+                          mode="single"
+                          selected={field.value}
+                          onSelect={field.onChange}
+                          disabled={(date) =>
+                            date > new Date() || date < new Date("1900-01-01")
+                          }
+                          initialFocus
+                        />
+                      </PopoverContent>
+                    </Popover>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="observaciones_cert"
+                render={({ field }) => (
+                  <FormItem className="">
+                    <FormLabel>Observaciones</FormLabel>
+                    <FormControl>
+                      <Textarea
+                        placeholder="Máximo 150 caracteres"
+                        className="resize-none"
+                        {...field}
                       />
-                    </PopoverContent>
-                  </Popover>
-                  <FormMessage />
-                </FormItem>
-
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="observaciones_cert"
-              render={({ field }) => (
-                <FormItem className="my-5">
-                  <FormLabel>Observaciones</FormLabel>
-                  <FormControl>
-                    <Textarea
-                      placeholder="Máximo 150 caracteres"
-                      className="resize-none"
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <Button type="submit" className="mt-3 mb-8 w-full">
-              <LogIn className="mr-2 h-4 w-4" /> Iniciar sesión
-            </Button>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </section>
+            <div className="flex my-8 justify-center">
+              <Button type="submit">
+                Agregar
+              </Button>
+            </div>
           </form>
         </Form>
       </section>
