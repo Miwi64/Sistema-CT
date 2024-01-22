9fe551ea3a604a5202caded41cb5b9d699c65417
Miwi64 <miargote@hotmail.com>
2024-01-21 17:15:17 -0800

    Changed display when there is not data available

--- frontend-vite/src/components/ui/data-table.tsx
+++ frontend-vite/src/components/ui/data-table.tsx
@@ -21,57 +21,59 @@ interface DataTableProps<TData> {
 export function DataTable<TData>({
   table,
 }: DataTableProps<TData>) {
-  return (
-      <div className="rounded-md mt-4 border">
-        <Table>
-          <TableHeader>
-            {table.getHeaderGroups().map((headerGroup) => (
-              <TableRow key={headerGroup.id}>
-                {headerGroup.headers.map((header) => {
-                  return (
-                    <TableHead key={header.id}>
-                      {header.isPlaceholder
-                        ? null
-                        : flexRender(
-                            header.column.columnDef.header,
-                            header.getContext()
-                          )}
-                    </TableHead>
-                  );
-                })}
-              </TableRow>
-            ))}
-          </TableHeader>
-          <TableBody>
-            {table.getRowModel().rows?.length ? (
-              table.getRowModel().rows.map((row) => (
-                <TableRow
-                  key={row.id}
-                  data-state={row.getIsSelected() && "selected"}
-                >
-                  {row.getVisibleCells().map((cell) => (
-                    <TableCell key={cell.id}>
-                      {flexRender(
-                        cell.column.columnDef.cell,
-                        cell.getContext()
+  return table.getRowModel().rows?.length ? (
+    <div className="rounded-md mt-4 border">
+      <Table>
+        <TableHeader>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <TableRow key={headerGroup.id}>
+              {headerGroup.headers.map((header) => {
+                return (
+                  <TableHead key={header.id}>
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                        header.column.columnDef.header,
+                        header.getContext()
                       )}
-                    </TableCell>
-                  ))}
-                </TableRow>
-              ))
-            ) : (
-              <TableRow>
-                <TableCell
-                  
-                  className="h-24 text-center"
-                >
-                  Sin datos para mostrar.
+                  </TableHead>
+                );
+              })}
+            </TableRow>
+          ))}
+        </TableHeader>
+        <TableBody>
+          {table.getRowModel().rows.map((row) => (
+            <TableRow
+              key={row.id}
+              data-state={row.getIsSelected() && "selected"}
+            >
+              {row.getVisibleCells().map((cell) => (
+                <TableCell key={cell.id}>
+                  {flexRender(
+                    cell.column.columnDef.cell,
+                    cell.getContext()
+                  )}
                 </TableCell>
-              </TableRow>
-            )}
-          </TableBody>
-        </Table>
-        <DataTablePagination table={table} />
-      </div>
+              ))}
+            </TableRow>
+          ))
+          }
+        </TableBody>
+      </Table>
+      <DataTablePagination table={table} />
+    </div>
+  ) : (
+    <div className="rounded-md mt-4 border">
+      <Table>
+        <TableBody>
+          <TableRow>
+            <TableCell className="h-24 text-center">
+              Sin datos para mostrar.
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </div>
   );
 }
