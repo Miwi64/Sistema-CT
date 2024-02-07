5dbd90e04dfdf8d70ab1d0c170760ad5a448d5d6
Miwi 64 <miargote@hotmail.com>
2024-02-06 23:26:38 -0800

    Minor corrections in date filters

--- backend/tasks/views.py
+++ backend/tasks/views.py
@@ -219,12 +219,12 @@ class AlumnoFilter(filters.FilterSet):
     titulo_fk = filters.NumberFilter(lookup_expr='exact')
     certificado_fk_null = filters.BooleanFilter(field_name='certificado_fk', lookup_expr='isnull')
     titulo_fk_null = filters.BooleanFilter(field_name='titulo_fk', lookup_expr='isnull')
-    nacimiento_max = django_filters.DateFilter(field_name='fecha_nacimiento', lookup_expr='gte')
-    nacimiento_min = django_filters.DateFilter(field_name='fecha_nacimiento', lookup_expr='lte')
-    registro_cert_max = django_filters.DateFilter(field_name='certificado_fk__fecha_registro', lookup_expr='gte')
-    registro_cert_min = django_filters.DateFilter(field_name='certificado_fk__fecha_registro', lookup_expr='lte')
-    registro_tit_max = django_filters.DateFilter(field_name='titulo_fk__fecha_registro', lookup_expr='gte')
-    registro_tit_min = django_filters.DateFilter(field_name='titulo_fk__fecha_registro', lookup_expr='lte')
+    nacimiento_max = django_filters.DateFilter(field_name='fecha_nacimiento', lookup_expr='lte')
+    nacimiento_min = django_filters.DateFilter(field_name='fecha_nacimiento', lookup_expr='gte')
+    registro_cert_max = django_filters.DateFilter(field_name='certificado_fk__fecha_registro', lookup_expr='lte')
+    registro_cert_min = django_filters.DateFilter(field_name='certificado_fk__fecha_registro', lookup_expr='gte')
+    registro_tit_max = django_filters.DateFilter(field_name='titulo_fk__fecha_registro', lookup_expr='lte')
+    registro_tit_min = django_filters.DateFilter(field_name='titulo_fk__fecha_registro', lookup_expr='gte')
 
     def filter_carrera_fk(self, queryset, name, value):
         values = [int(val) for val in value.split(',')]
