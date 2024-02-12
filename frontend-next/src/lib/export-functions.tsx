b7f0ea769460804cb596ecc24666f673e004193a
Miwi 64 <miargote@hotmail.com>
2024-02-12 15:36:58 -0800

    New excel export format method

--- frontend-next/src/lib/export-functions.tsx
+++ frontend-next/src/lib/export-functions.tsx
@@ -70,29 +70,11 @@ export const generateGobReport = async (template: ArrayBuffer, data: GobReportDa
 
 pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
-//CSV
-function jsonToCsv(headers: string[], columns: string[], jsonData: Student[]) {
-    let csvContent = headers.join(",") + "\n";
-    jsonData.forEach((item) => {
-        let values = columns
-            .map((key) => {
-                const cell = item[key] || "";
-                return typeof cell === "string"
-                    ? `"${cell.replace(/"/g, '""')}"`
-                    : cell;
-            })
-            .join(",");
-        csvContent += values + "\n";
-    });
-
-    return csvContent;
-}
-
 const filterProps = (obj: { [x: string]: any }, props: string[]) => {
     const newObj: { [x: string]: any } = {};
-    props.forEach(prop => {
+    props.forEach((prop, index) => {
         if (obj.hasOwnProperty(prop)) {
-            newObj[prop] = obj[prop];
+            newObj[`column${index}`] = obj[prop];
         }
     });
     return newObj;
@@ -103,108 +85,91 @@ const formatHeader = (refStrings: { [x: string]: string }, array: string[]) => {
     return newArray;
 }
 
-
-/*export const handleExcelDownload = async (urlFilter: string, columnVisibility: VisibilityState) => {
-    const columns = Object.keys(columnVisibility).filter(col => columnVisibility[col] === true);
-    const headers = formatHeader(REF_COLUMN_NAMES, columns);
-
-    fetch("http://127.0.0.1:8000/alumnos/?" + `${urlFilter}`)
-        .then((response) => response.json())
-        .then((data) => {
-            const selected_data = data.map((element: { [x: string]: any }) => filterProps(element, columns));
-            const csv = jsonToCsv(headers, columns, selected_data);
-            const blob = new Blob([csv], { type: "text/csv" });
-            const url = window.URL.createObjectURL(blob);
-            const a = document.createElement("a");
-            a.style.display = "none";
-            a.href = url;
-            a.download = "archivo.csv";
-            document.body.appendChild(a);
-            a.click();
-            document.body.removeChild(a);
-            window.URL.revokeObjectURL(url);
-        });
-};*/
-
 export const handleExcelDownload = async (urlFilter: string, columnVisibility: VisibilityState) => {
     const columns = Object.keys(columnVisibility).filter(col => columnVisibility[col] === true);
+    const headers = formatHeader(REF_COLUMN_NAMES, columns);
+    const objHeaders: { [x: string]: string } = {};
+    headers.forEach((head, index) => {
+        objHeaders[`column${index + 1}`] = head
+    });
     fetch("http://127.0.0.1:8000/alumnos/?" + `${urlFilter}`)
         .then((response) => response.json())
-        .then(async (data) => {
+        .then(async (data: Student[]) => {
             const selected_data = data.map((element: { [x: string]: any }) => filterProps(element, columns));
             const fetchTemplate = await fetch(`http://localhost:3000/templates/export_template.xlsx`);
-            const template = await fetchTemplate.arrayBuffer()
-            const excelTemplate = await JsExcelTemplate.fromArrayBuffer(template)
-            excelTemplate.set("data", data);
-            const blob = await excelTemplate.toBlob()
-            saveAs(blob, "table.xlsx")
+            const template = await fetchTemplate.arrayBuffer();
+            const excelTemplate = await JsExcelTemplate.fromArrayBuffer(template);
+            excelTemplate.set("headers", [objHeaders]);
+            excelTemplate.set("data", selected_data);
+            const blob = await excelTemplate.toBlob();
+            saveAs(blob, "table.xlsx");
         });
-    }
+}
 
 
-    export const handlePdfDownload = async (urlFilter: string, columnVisibility: VisibilityState) => {
-        const columns = Object.keys(columnVisibility).filter(col => columnVisibility[col] === true);
-        const headers = formatHeader(REF_COLUMN_NAMES, columns);
-        const datos = await fetch("http://127.0.0.1:8000/alumnos/?" + `${urlFilter}`);
-        const data = await datos.json();
-        const docDefinition: TDocumentDefinitions = {
-            pageSize: 'A4',
-            pageOrientation: 'portrait',
-            content: [
-                {
-                    table: {
-                        body: [
-                            headers.map(header => ({ text: header, style: 'head', border: [true, true, true, true] })),
-                            ...data.map((row: Student) => (
-                                columns.map(column => ({ text: row[column], style: 'body', border: [true, true, true, true] }))
-                            )),
-                        ],
-                        headerRows: 1,
-                        dontBreakRows: true,
+export const handlePdfDownload = async (urlFilter: string, columnVisibility: VisibilityState) => {
+    const columns = Object.keys(columnVisibility).filter(col => columnVisibility[col] === true);
+    const headers = formatHeader(REF_COLUMN_NAMES, columns);
+    const fetchData = await fetch("http://127.0.0.1:8000/alumnos/?" + `${urlFilter}`);
+    const data = await fetchData.json();
+    const docDefinition: TDocumentDefinitions = {
+        pageSize: 'A4',
+        pageOrientation: 'portrait',
+        content: [
+            {
+                table: {
+                    body: [
+                        headers.map(header => ({ text: header, style: 'head', border: [true, true, true, true] })),
+                        ...data.map((row: Student) => (
+                            columns.map(column => ({ text: row[column], style: 'body', border: [true, true, true, true] }))
+                        )),
+                    ],
+                    headerRows: 1,
+                    dontBreakRows: true,
+                },
+                layout: {
+                    hLineWidth: function (i, node) {
+                        return 0.5; // Set the width of horizontal lines
                     },
-                    layout: {
-                        hLineWidth: function (i, node) {
-                            return 0.5; // Set the width of horizontal lines
-                        },
-                        vLineWidth: function (i, node) {
-                            return 0.5; // Set the width of vertical lines
-                        },
-                        hLineColor: function (i, node) {
-                            return '#aaa'; // Set the color of horizontal lines
-                        },
-                        vLineColor: function (i, node) {
-                            return '#aaa'; // Set the color of vertical lines
-                        },
-                        paddingLeft: function (i, node) {
-                            return 2; // Set the left padding
-                        },
-                        paddingRight: function (i, node) {
-                            return 2; // Set the right padding
-                        },
-                        paddingTop: function (i, node) {
-                            return 2; // Set the top padding
-                        },
-                        paddingBottom: function (i, node) {
-                            return 2; // Set the bottom padding
-                        },
+                    vLineWidth: function (i, node) {
+                        return 0.5; // Set the width of vertical lines
+                    },
+                    hLineColor: function (i, node) {
+                        return '#aaa'; // Set the color of horizontal lines
+                    },
+                    vLineColor: function (i, node) {
+                        return '#aaa'; // Set the color of vertical lines
+                    },
+                    paddingLeft: function (i, node) {
+                        return 2; // Set the left padding
+                    },
+                    paddingRight: function (i, node) {
+                        return 2; // Set the right padding
+                    },
+                    paddingTop: function (i, node) {
+                        return 2; // Set the top padding
+                    },
+                    paddingBottom: function (i, node) {
+                        return 2; // Set the bottom padding
                     },
                 },
-            ],
-            styles: {
-                header: {
-                    bold: true,
-                    fontSize: 16,
-                    alignment: 'center',
-                    margin: [0, 0, 0, 10],
-                },
-                body: {
-                    fontSize: 5,
-                },
-                head: {
-                    alignment: 'center',
-                    fontSize: 5,
-                }
             },
-        };
-        pdfMake.createPdf(docDefinition).download('data-table.pdf');
-    }
+        ],
+        styles: {
+            header: {
+                bold: true,
+                fontSize: 16,
+                alignment: 'center',
+                margin: [0, 0, 0, 10],
+            },
+            body: {
+                fontSize: 5,
+            },
+            head: {
+                alignment: 'center',
+                fontSize: 5,
+            }
+        },
+    };
+    pdfMake.createPdf(docDefinition).download('data-table.pdf');
+}
