282312542680a9a738e41f3385291df464e17b57
Andre Cadena <al19760606@ite.edu.mx>
2024-02-27 12:29:03 -0800

    Fixed Type Errors

--- frontend-next/src/app/table/certificate-form/page.tsx
+++ frontend-next/src/app/table/certificate-form/page.tsx
@@ -1,24 +1,22 @@
-import { authOptions } from '@/app/api/auth/[...nextauth]/route'
-import CertificateForm from '@/components/forms/certificate-form'
-import { getServerSession } from 'next-auth'
-import { Session } from 'next-auth'
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import CertificateForm from "@/components/forms/certificate-form";
+import { getServerSession } from "next-auth";
+import { Session } from "next-auth";
 
 const getCareers = async (session: Session) => {
-  const fetchApi =
-    await fetch(`http://127.0.0.1:8000/data/api/v1/carreras/`,
-      {
-        method: "GET",
-        headers: {
-          Authorization: "Token " + session.token,
-        }
-      })
-  const results = await fetchApi.json()
-  return results
-}
+  const fetchApi = await fetch(`http://127.0.0.1:8000/data/api/v1/carreras/`, {
+    method: "GET",
+    headers: {
+      Authorization: "Token " + session.token,
+    },
+  });
+  const results = await fetchApi.json();
+  return results;
+};
 
 const Page = async () => {
-  const session = await getServerSession(authOptions)
-  const careers = await getCareers(session)
+  const session = await getServerSession(authOptions);
+  const careers = await getCareers(session);
   return (
     <>
       <h1 className="my-5 text-2xl font-semibold leading-none tracking-tight">
@@ -26,7 +24,7 @@ const Page = async () => {
       </h1>
       <CertificateForm careers={careers} session={session} />
     </>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
